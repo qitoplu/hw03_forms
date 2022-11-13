42587c5bd25f085ff707100b98de6bd0ca389058
Вячеслав Петров <213xixixi@gmail.com>
2022-11-13 16:31:01 +0300

    done1

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -1,7 +1,8 @@
 from datetime import date
+
+
 def year(request):
     """Добавляет переменную с текущим годом."""
     return {
-        'year':date.today().year
+        'year': date.today().year
     }
-    
