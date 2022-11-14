8e33813a42c0b7666105120665f507b3e8c3a049
Вячеслав Петров <213xixixi@gmail.com>
2022-11-14 15:23:52 +0300

    done11

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -7,44 +7,37 @@ POSTS_QUANTITY = 10
 CHAR_THIRTY = 30
 
 
-def index(request):
-    post_list = Post.objects.all().order_by('-pub_date')
+def paginatorr(post_list, request):
     paginator = Paginator(post_list, POSTS_QUANTITY)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+    return page_obj
+
+
+def index(request):
     context = {
-        'page_obj': page_obj,
+        'page_obj': paginatorr(
+            Post.objects.select_related('group', 'author'), request)
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:POSTS_QUANTITY]
-    post_list = Post.objects.all().order_by('-pub_date')
-    paginator = Paginator(post_list, POSTS_QUANTITY)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    posts = group.posts.select_related('author')
     context = {
         'groups': group,
-        'posts': posts,
-        'page_obj': page_obj
+        'page_obj': paginatorr(posts, request)
     }
     return render(request, 'posts/group_list.html', context)
 
 
 def profile(request, username):
     author = get_object_or_404(User, username=username)
-    post_list = author.posts.all()
-    posts_count = post_list.count()
-    paginator = Paginator(post_list, POSTS_QUANTITY)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    posts = author.posts.all()
     context = {
         'author': author,
-        'post_list': post_list,
-        'posts_count': posts_count,
-        'page_obj': page_obj,
+        'page_obj': paginatorr(posts, request)
     }
     template = 'posts/profile.html'
     return render(request, template, context)
@@ -52,15 +45,9 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    post_title = post.text[:CHAR_THIRTY]
-    pub_date = post.pub_date
     author = post.author
-    posts_author = author.posts.all().count()
     context = {
         'post': post,
-        'post_title': post_title,
-        'pub_date': pub_date,
-        'posts_author': posts_author,
         'author': author,
     }
     return render(request, 'posts/post_detail.html', context)
@@ -75,7 +62,6 @@ def post_create(request):
             post.author = request.user
             post.save()
             return redirect('posts:profile', post.author)
-    form = PostForm()
     return render(
         request,
         'posts/create_post.html',
@@ -88,14 +74,10 @@ def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if post.author != request.user:
         return redirect('posts:post_detail', post_id)
-    if request.method == 'POST':
-        form = PostForm(request.POST or None)
-        if form.is_valid():
-            post.text = form.cleaned_data['text']
-            post.group = form.cleaned_data['group']
-            post.save()
-            return redirect('posts:post_detail', post_id)
-    form = PostForm()
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        post.save()
+        return redirect('posts:post_detail', post_id)
     return render(
         request,
         'posts/create_post.html',
