95b1069334a104e740f87de2c20fa86168b2b8a8
Вячеслав Петров <213xixixi@gmail.com>
2022-11-14 10:02:40 +0300

    done10

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -15,7 +15,7 @@ app_name = 'users'
 urlpatterns = [
     path(
         'logout/',
-        LogoutView.as_view(template_name='users./logged_out.html'),
+        LogoutView.as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
     path(
